5f475a136c0f0c92255ff2d57cf6679c43267860
Kodylow <Kodylow@users.noreply.github.com>
2022-12-30 01:58:47 +0000

    deploy: 6b01d8f93c8b166073b383cb7ac7c702154d71ce

--- source-files.js
+++ source-files.js
@@ -1,4 +1,5 @@
 var sourcesIndex = JSON.parse('{\
+"aead":["",[],["lib.rs"]],\
 "configgen":["",[],["configgen.rs"]],\
 "distributedgen":["",[],["distributedgen.rs"]],\
 "fedimint_api":["",[["core",[],["client.rs","encode.rs","server.rs"]],["db",[],["mem_impl.rs","mod.rs"]],["encoding",[],["btc.rs","mod.rs","secp256k1.rs","tbs.rs"]],["module",[],["audit.rs","interconnect.rs","mod.rs","registry.rs"]],["net",[["peers",[],["fake.rs"]]],["mod.rs","peers.rs"]]],["backup.rs","bitcoin_rpc.rs","cancellable.rs","config.rs","core.rs","lib.rs","macros.rs","task.rs","tiered.rs","tiered_multi.rs"]],\
@@ -9,18 +10,20 @@ var sourcesIndex = JSON.parse('{\
 "fedimint_core":["",[],["config.rs","epoch.rs","lib.rs","outcome.rs","transaction.rs"]],\
 "fedimint_dbdump":["",[],["main.rs"]],\
 "fedimint_derive":["",[],["lib.rs"]],\
+"fedimint_derive_secret":["",[],["lib.rs"]],\
 "fedimint_ln":["",[["contracts",[],["account.rs","incoming.rs","mod.rs","outgoing.rs"]]],["common.rs","config.rs","db.rs","lib.rs"]],\
 "fedimint_mint":["",[],["common.rs","config.rs","db.rs","lib.rs"]],\
 "fedimint_rocksdb":["",[],["lib.rs"]],\
 "fedimint_server":["",[["consensus",[],["debug.rs","interconnect.rs","mod.rs"]],["net",[],["api.rs","connect.rs","framed.rs","mod.rs","peers.rs","queue.rs"]]],["config.rs","db.rs","lib.rs","multiplexed.rs","rng.rs"]],\
 "fedimint_sled":["",[],["lib.rs"]],\
+"fedimint_sqlite":["",[],["lib.rs"]],\
 "fedimint_testing":["",[["btc",[],["bitcoind.rs","fixtures.rs","mod.rs"]]],["lib.rs"]],\
 "fedimint_wallet":["",[],["common.rs","config.rs","db.rs","keys.rs","lib.rs","tweakable.rs","txoproof.rs"]],\
 "fedimintd":["",[["ui",[],["configgen.rs","mod.rs"]]],["encrypt.rs","lib.rs"]],\
 "gateway_cli":["",[],["main.rs"]],\
 "hkdf":["",[],["lib.rs"]],\
 "ln_gateway":["",[["rpc",[],["mod.rs","rpc_client.rs","rpc_server.rs"]]],["actor.rs","client.rs","cln.rs","config.rs","lib.rs","ln.rs","utils.rs"]],\
-"mint_client":["",[["ln",[],["db.rs","incoming.rs","mod.rs","outgoing.rs"]],["mint",[],["backup.rs","db.rs","mod.rs"]],["wallet",[],["db.rs","mod.rs"]]],["api.rs","db.rs","lib.rs","query.rs","secrets.rs","transaction.rs","utils.rs"]],\
+"mint_client":["",[["ln",[],["db.rs","incoming.rs","mod.rs","outgoing.rs"]],["mint",[],["backup.rs","db.rs","mod.rs"]],["wallet",[],["db.rs","mod.rs"]]],["api.rs","db.rs","lib.rs","query.rs","transaction.rs","utils.rs"]],\
 "tbs":["",[["serde_impl",[],["mod.rs","scalar.rs"]]],["hash.rs","lib.rs","poly.rs"]]\
 }');
 createSourceSidebar();
