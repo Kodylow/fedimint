aed7277e38b04b0847e516210cd235aafb36fcc2
Kodylow <Kodylow@users.noreply.github.com>
2023-04-15 14:09:36 +0000

    deploy: aa21c66582c17a68f19438366864652cba4bd590

---
+++ implementors/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {
+"dbtool":[["impl Subcommand for <a class=\"enum\" href=\"dbtool/enum.DbCommand.html\" title=\"enum dbtool::DbCommand\">DbCommand</a>"]],
+"fedimint_bin_tests":[["impl Subcommand for <a class=\"enum\" href=\"fedimint_bin_tests/enum.Cmd.html\" title=\"enum fedimint_bin_tests::Cmd\">Cmd</a>"]],
+"fedimint_cli":[["impl Subcommand for <a class=\"enum\" href=\"fedimint_cli/enum.Command.html\" title=\"enum fedimint_cli::Command\">Command</a>"]],
+"fedimintd":[["impl Subcommand for <a class=\"enum\" href=\"fedimintd/distributed_gen/enum.Command.html\" title=\"enum fedimintd::distributed_gen::Command\">Command</a>"]],
+"fixtures":[["impl Subcommand for <a class=\"enum\" href=\"fixtures/enum.Cmd.html\" title=\"enum fixtures::Cmd\">Cmd</a>"]],
+"gateway_cli":[["impl Subcommand for <a class=\"enum\" href=\"gateway_cli/enum.Commands.html\" title=\"enum gateway_cli::Commands\">Commands</a>"]],
+"ln_gateway":[["impl Subcommand for <a class=\"enum\" href=\"ln_gateway/enum.LightningMode.html\" title=\"enum ln_gateway::LightningMode\">LightningMode</a>"]],
+"recoverytool":[["impl Subcommand for <a class=\"enum\" href=\"recoverytool/enum.TweakSource.html\" title=\"enum recoverytool::TweakSource\">TweakSource</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
