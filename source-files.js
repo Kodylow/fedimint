e3c556e7ec18c16d29b5fa5e84902429ffaa9501
Kodylow <Kodylow@users.noreply.github.com>
2023-08-09 23:22:57 +0000

    deploy: 05a03c8f02a31ad5c72c434e5087ac2c0fd98219

--- source-files.js
+++ source-files.js
@@ -1,5 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"dbtool":["",[],["dump.rs","main.rs"]],\
 "devimint":["",[],["external.rs","federation.rs","lib.rs","util.rs","vars.rs"]],\
 "faucet":["",[],["faucet.rs"]],\
 "fedimint_aead":["",[],["lib.rs"]],\
@@ -10,7 +9,8 @@ var sourcesIndex = JSON.parse('{\
 "fedimint_cli":["",[],["main.rs"]],\
 "fedimint_client":["",[["module",[],["gen.rs","mod.rs"]],["sm",[],["dbtx.rs","executor.rs","mod.rs","notifier.rs","state.rs","util.rs"]],["transaction",[],["builder.rs","mod.rs","sm.rs"]]],["backup.rs","db.rs","lib.rs","oplog.rs","secret.rs"]],\
 "fedimint_client_legacy":["",[["api",[],["fake.rs"]],["ln",[],["db.rs","incoming.rs","mod.rs","outgoing.rs"]],["mint",[],["backup.rs","db.rs","mod.rs"]],["wallet",[],["db.rs","mod.rs"]]],["api.rs","db.rs","lib.rs","outcome.rs","transaction.rs","utils.rs"]],\
-"fedimint_core":["",[["core",[],["backup.rs","client.rs","server.rs"]],["db",[],["mem_impl.rs","mod.rs","notifications.rs"]],["encoding",[],["btc.rs","mod.rs","secp256k1.rs","tbs.rs","tls.rs"]],["hex",[],["mod.rs","serde.rs"]],["module",[],["audit.rs","mod.rs","registry.rs"]],["net",[["peers",[],["fake.rs"]]],["mod.rs","peers.rs"]],["util",[],["broadcaststream.rs","mod.rs"]]],["admin_client.rs","api.rs","backup.rs","bitcoinrpc.rs","cancellable.rs","config.rs","core.rs","epoch.rs","fmt_utils.rs","lib.rs","macros.rs","outcome.rs","query.rs","task.rs","tiered.rs","tiered_multi.rs","time.rs","timing.rs","transaction.rs","txoproof.rs"]],\
+"fedimint_core":["",[["core",[],["backup.rs","client.rs","server.rs"]],["db",[],["mem_impl.rs","mod.rs","notifications.rs"]],["encoding",[],["as_hex.rs","btc.rs","mod.rs","secp256k1.rs","tbs.rs","tls.rs"]],["hex",[],["mod.rs","serde.rs"]],["module",[],["audit.rs","mod.rs","registry.rs","version.rs"]],["net",[["peers",[],["fake.rs"]]],["mod.rs","peers.rs"]],["util",[],["broadcaststream.rs","mod.rs"]]],["admin_client.rs","api.rs","backup.rs","bitcoinrpc.rs","cancellable.rs","config.rs","core.rs","epoch.rs","fmt_utils.rs","lib.rs","macros.rs","outcome.rs","query.rs","task.rs","tiered.rs","tiered_multi.rs","time.rs","timing.rs","transaction.rs","txoproof.rs"]],\
+"fedimint_dbtool":["",[],["dump.rs","main.rs"]],\
 "fedimint_derive":["",[],["lib.rs"]],\
 "fedimint_derive_secret":["",[],["lib.rs"]],\
 "fedimint_dummy_client":["",[],["api.rs","db.rs","lib.rs","states.rs"]],\
@@ -21,22 +21,23 @@ var sourcesIndex = JSON.parse('{\
 "fedimint_ln_server":["",[],["lib.rs"]],\
 "fedimint_load_test_tool":["",[],["common.rs","main.rs"]],\
 "fedimint_logging":["",[],["lib.rs"]],\
+"fedimint_metrics":["",[],["lib.rs"]],\
 "fedimint_mint_client":["",[["backup",[],["recovery.rs"]]],["backup.rs","db.rs","input.rs","lib.rs","oob.rs","output.rs"]],\
 "fedimint_mint_common":["",[],["common.rs","config.rs","db.rs","lib.rs"]],\
 "fedimint_mint_server":["",[],["lib.rs"]],\
 "fedimint_rocksdb":["",[],["lib.rs"]],\
 "fedimint_server":["",[["config",[],["api.rs","distributedgen.rs","io.rs","mod.rs"]],["consensus",[],["debug.rs","mod.rs","server.rs"]],["net",[],["api.rs","connect.rs","framed.rs","mod.rs","peers.rs","queue.rs"]]],["db.rs","lib.rs","multiplexed.rs"]],\
-"fedimint_sqlite":["",[],["lib.rs"]],\
 "fedimint_testing":["",[["btc",[],["mock.rs","mod.rs","real.rs"]],["ln",[],["mock.rs","mod.rs","real.rs"]]],["db.rs","federation.rs","fixtures.rs","gateway.rs","lib.rs"]],\
-"fedimint_wallet_client":["",[],["api.rs","deposit.rs","lib.rs","withdraw.rs"]],\
+"fedimint_wallet_client":["",[],["api.rs","db.rs","deposit.rs","lib.rs","withdraw.rs"]],\
 "fedimint_wallet_common":["",[],["config.rs","db.rs","keys.rs","lib.rs","tweakable.rs","txoproof.rs"]],\
 "fedimint_wallet_server":["",[],["lib.rs"]],\
+"fedimint_wasm_tests":["",[],["lib.rs"]],\
 "fedimintd":["",[],["fedimintd.rs","lib.rs"]],\
 "gateway_cli":["",[],["main.rs"]],\
 "gateway_cln_extension":["",[],["cln_extension.rs"]],\
 "gatewayd":["",[],["gatewayd.rs"]],\
 "hkdf":["",[],["lib.rs"]],\
-"ln_gateway":["",[["ng",[],["mod.rs","pay.rs","register.rs"]],["rpc",[],["mod.rs","rpc_client.rs","rpc_server.rs"]]],["client.rs","db.rs","lib.rs","lnd.rs","lnrpc_client.rs","types.rs","utils.rs"]],\
+"ln_gateway":["",[["ng",[],["complete.rs","mod.rs","pay.rs"]],["rpc",[],["mod.rs","rpc_client.rs","rpc_server.rs"]]],["client.rs","db.rs","lib.rs","lnd.rs","lnrpc_client.rs","types.rs","utils.rs"]],\
 "recoverytool":["",[],["main.rs"]],\
 "tbs":["",[["serde_impl",[],["mod.rs","scalar.rs"]]],["hash.rs","lib.rs","poly.rs"]]\
 }');
